ff6b7d3fe99b4e37b08aa275ee89a0b17768f6d8
Adnan Shihabi <adnan@ashihabi.me>
2024-02-17 13:05:17 -0800

    Development Updates: - addressed lag issues on larger screens   - backgroundVisuals.js was causing lower framerates   - set `pixelDensity(1)` (p5js)

--- public/javascripts/backgroundVisual.js
+++ public/javascripts/backgroundVisual.js
@@ -5,6 +5,7 @@ let basePixels = 14;
 let timeOffset = 0.01;
 
 function setup () {
+  pixelDensity(1);
   frameRate(12);
   colorMode(RGBA, 255);
   canvas = createCanvas(windowWidth + 20, windowHeight + 20);
